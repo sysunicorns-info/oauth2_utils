7333c1a634ef2dccf9166bae278e5a5ae4152b1f
MirageCentury <victorien.vanroye@gmail.com>
2020-08-21 17:08:00 +0200

    Fix Startup

--- src/backend_main.py
+++ src/backend_main.py
@@ -5,4 +5,4 @@ from backend.application import Application
 application = Application()
 
 if __name__ == "__main__":
-    uvicorn.run("backend:application", host="127.0.0.1", port=5000, log_level="debug")
+    uvicorn.run("backend_main:application", host="127.0.0.1", port=5000, log_level="debug")
